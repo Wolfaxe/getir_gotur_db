cbeba7e7fa9876c84b8ed718ae13af34de17c989
Wolfaxe <berkayakyolal8@gmail.com>
2025-08-09 01:08:56 +0300

    Delete market ve dağıtım yerleri.sql

--- market ve dağıtım yerleri.sql
+++
@@ -1,6 +0,0 @@
-SELECT name, shop, amenity, building, way
-FROM planet_osm_point
-WHERE shop IS NOT NULL
-   OR amenity IN ('cafe','food_court','ice_cream','marketplace','post_office')
-   OR building IN ('apartments','hotel','hospital','house','office','school','university'
-    );
